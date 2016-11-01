3e8ed349b2e48ce2f55fc30be1620a3a463746b8
iamladyyumyum <ntraylor@outlook.com>
2016-11-01 22:39:55 +0000

    Got navbar to work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+ 
+  def about
+  end
+ 
+  def playon
+  end
+
+  def celebrate
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
-
+  get 'home/about'
+  get 'home/playon'
+  get 'home/celebrate'
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
